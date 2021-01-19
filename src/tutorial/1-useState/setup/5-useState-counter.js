fd346609c6b19924e6e2a14e7e7540f57b94108b
alitemel89 <64444827+alitemel89@users.noreply.github.com>
2021-01-19 13:54:06 +0300

    add counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,45 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const increment = () => {
+    setValue(value + 1);
+  }
+
+  const decrement = () => {
+    if (value > 0) {
+      setValue(value - 1);
+    }
+  }
+
+  const resetCounter = () => {
+    setValue(0)
+  }
+
+  const complexIncreaser = () => {
+    setTimeout(() => {
+      setValue(value + 1)
+    }, 2000)
+  }
+
+  return (
+    <React.Fragment>
+      <section style={{margin: '4rem 0'}}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={increment}>increase</button>
+        <button className="btn" onClick={resetCounter}>reset</button>
+        <button className="btn" onClick={decrement}>decrease</button>
+      </section>
+      <section style={{margin: '4rem 0'}}>
+        <h2>More Complex Counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncreaser}>increase later</button>
+      </section>
+     
+    </React.Fragment>
+  )
 };
 
 export default UseStateCounter;
